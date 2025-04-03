dae28919e521e2f9e95dd74d3c0226c5e1045fb6
Rachel Rondon <rachel.sun.rondon@gmail.com>
2025-04-03 14:32:21 -0400

    feat: update server.py file to generate 10/10 score

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
+"""
+server.py
+
+This module handles the web server setup and routes for the emotion detection application.
+"""
+
 from flask import Flask, request, jsonify, render_template
-import requests 
-import json 
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    """
+    renders the home page
+    """
     return render_template('index.html')
 
 @app.route('/emotionDetector', methods=['GET'])
 def emotion_detector_route():
+    """
+    returns a response for the emotion detector 
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     print(f"Received text: {text_to_analyze}")
 
@@ -20,23 +31,28 @@ def emotion_detector_route():
         # Determine the dominant emotion
         dominant_emotion = max(emotions, key=emotions.get, default=None)
         emotions["dominant_emotion"] = dominant_emotion
-        
+
         return jsonify({
             'For the given statement, the system response is': emotions,
             'The dominant emotion is': dominant_emotion
-        })  
-    else:
-        print("No text provided, returning error response.")  # Debugging message
-        return {
-           "message": "Invalid text! Please provide text to analyze."
-        }
+        })
+
+    print("No text provided, returning error response.")  # Debugging message
+    return {
+        "message": "Invalid text! Please provide text to analyze."
+    }
 
 def perform_emotion_detection(text):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    """
+    returns the score for each emotion
+    """
+    start = 'https://sn-watson-emotion.labs.skills.network/v1/'
+    end = 'watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = f'{start}{end}'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text} }
 
-    response = requests.post(url, headers=headers, json=input_json)
+    response = requests.post(url, headers=headers, json=input_json, timeout=10)
 
     if response.status_code != 200:
         return {
@@ -46,8 +62,8 @@ def perform_emotion_detection(text):
             'joy': 0, 
             'sadness': 0
         }
-    
-    try: 
+
+    try:
         data = response.json()
         emotion_predictions = data['emotionPredictions']
         score = emotion_predictions[0]['emotion']
@@ -65,8 +81,8 @@ def perform_emotion_detection(text):
             'joy': joy_score,
             'sadness': sadness_score
         }
-    
-    except (KeyError, IndexError, ValueError) as e:
+
+    except (KeyError, IndexError, ValueError):
 
         return {
             'anger': 0, 
